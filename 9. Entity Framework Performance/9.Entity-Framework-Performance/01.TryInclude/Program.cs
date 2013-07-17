0d4749e3b2ca9951c5092ffef215e1cb9d5d79ab
skirov <stoian.kiroff@gmail.com>
2013-07-18 00:27:50 +0300

    Commit perf homework

---
+++ 9. Entity Framework Performance/9.Entity-Framework-Performance/01.TryInclude/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using EntityFramework.Data;
+
+namespace _01.TryInclude
+{
+    class Program
+    {
+        static TelerikAcademyEntities context = new TelerikAcademyEntities();
+
+        static void GetInfoWithoutInclude()
+        {
+            foreach (var employee in context.Employees)
+            {
+                Console.WriteLine("{0} {1}", employee.FirstName, employee.LastName);
+                Console.WriteLine("{0}", employee.Department);
+                Console.WriteLine("{0}", employee.Address.Town.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine(context.Employees.Count());
+        }
+
+        static void GetInfoWithInclude()
+        {
+            foreach (var employee in context.Employees.Include("Address"))
+            {
+                Console.WriteLine("{0} {1}", employee.FirstName, employee.LastName);
+                Console.WriteLine("{0}", employee.Department);
+                Console.WriteLine("{0}", employee.Address.Town.Name);
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            GetInfoWithInclude();
+            //GetInfoWithoutInclude();
+        }
+    }
+}
---
+++ 9. Entity Framework Performance/9.Entity-Framework-Performance/02.ToListPerformance/Program.cs
@@ -0,0 +1,25 @@
+﻿using EntityFramework.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ToListPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelerikAcademyEntities context = new TelerikAcademyEntities();
+
+            //var allEmployees = context.Employees.ToList()
+            //                          .Select(e => e.Address).ToList()
+            //                          .Select(e => e.Town).ToList()
+            //                          .Where(e => e.Name == "Sofia");
+
+            var allEmployeesOptimized = context.Employees
+                                               .Select(e => e.Address)
+                                               .Select(e => e.Town)
+                                               .Where(e => e.Name == "Sofia").ToList();
+        }
+    }
+}
